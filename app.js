e89895e9a18593628ae21d5ce43734e55eee3c0e
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-21 21:19:52 -0700

    javascript

---
+++ app.js
@@ -0,0 +1,9 @@
+document.write("<span>The Age calculator </span><br /> <br />")
+let birthYear=+prompt("Type your  birth Year")
+let currentyear=+prompt("type your current year")
+document.write("<br />Date of birth day"+birthYear)
+document.write("<br />current year"+currentyear+"<br />")
+
+let age=(birthYear -currentyear)
+let or=(age++)
+document.write("<br />Ther are either"+age+"or"+or+"year old")
